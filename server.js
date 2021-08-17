a1446958a17134937695be1b1bd18240241edd87
Richard Miller <richardmmill@gmail.com>
2021-08-17 11:54:38 -0400

    second commit

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const app = express();
 
 const path = require('path');
 
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: 'bcbabf4b76004681aa03e99652d0cd7e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 app.use(express.json());
 
 const port = process.env.PORT || 4040;
 
+// rollbar.log('rollbar')
+
 app.get('/', function(req, res) {
+    rollbar.log('App.get homepage hit')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
